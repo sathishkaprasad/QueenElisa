f181234293cf1e5881787e8093e64b25f877edb5
sathishkaprasad <wpbot1520@gmail.com>
2024-10-24 03:02:24 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = {
-  SESSION_ID: '',
+  SESSION_ID: 'QUEENELISA;;;==QRt0SZVhjZYdlQ41UYJdFauNUUzwmMnp1SM50TOd3bVhDMFhVSVpkc5g3bjIFTZRVaxQGNpwd=23056976749',
   ALIVE_REACT: '👋',
   ALIVE_MESSAGE: `*Hello @name [ And @gname @members Number of Members ] I am Alive Now !*
 
